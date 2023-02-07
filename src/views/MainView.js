eaf9d716f36e09be4db5776d6f34a10f1e3cb6b1
nacho503 <ignacio.rosenberg.503@gmail.com>
2023-02-07 18:31:53 -0300

    Arreglos en responsive

--- src/views/MainView.js
+++ src/views/MainView.js
@@ -7,10 +7,14 @@ import bkg from "../img/bkg.jpg";
 //Controls Container
 const ControlsContainer = styled.div`
   background-color: black;
+  width: 100%;
   height: 80px;
   display: flex;
   justify-content: space-around;
   margin-bottom: 10rem;
+  @media (max-width: 768px) {
+    margin-bottom: 5rem;
+  }
 `;
 
 const Blogo = styled.h1`
@@ -25,6 +29,10 @@ const BattleGridsContainer = styled.div`
   display: flex;
   justify-content: space-evenly;
   /* background-image: url(${bkg}); */
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+  }
 `;
 
 const ControlsContainerView = () => {
